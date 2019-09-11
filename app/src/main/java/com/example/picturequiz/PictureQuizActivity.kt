4f72f4f31780f7e597c8249ba667a93136fb7050
kim-ly <SoSe2018>
2019-09-11 12:02:48 +0200

    Level 1 example

---
+++ app/src/main/java/com/example/picturequiz/PictureQuizActivity.kt
@@ -0,0 +1,36 @@
+package com.example.picturequiz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class PictureQuizActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_main)
+
+        // Using the id given in the layout file you can access the component.
+        // Set an action when the user clicks on the confirm button.
+        button.setOnClickListener {
+            checkAnswer()
+        }
+        }
+
+    /**
+     * Check if the submitted answer is correct.
+     */
+    private fun checkAnswer() {
+        val answer = editText.text.toString()
+
+        // When the answer equals "giraffe" then display a correct message using a toast message.
+        // Otherwise display an incorrect message.
+        if (answer == getString(R.string.giraffe)) {
+            Toast.makeText(this, getString(R.string.correct), Toast.LENGTH_LONG).show()
+        } else {
+            Toast.makeText(this, getString(R.string.incorrect), Toast.LENGTH_LONG).show()
+        }
+    }
+
+}
